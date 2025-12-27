728bdbe61f28e1cc74caabbb00fdc73f6cfcb2a4
unknown <cuzawzawmyint@gmail.com>
2025-12-27 13:42:51 +0700

    fix prisma client error on deploy render

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/prisma/index.js";
+import pkg from "@prisma/client";
+const { PrismaClient } = pkg;
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
